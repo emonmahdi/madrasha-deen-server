b9b63ba53d59cdfe56d1a8efed8722a174a41b30
emonmahdi <developeremon1997@gmail.com>
2025-11-11 16:05:28 +0600

    connected mongodb server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,9 +12,41 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ebn1vec.mongodb.net/madrashaDB?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Madrasha Deen server open ");
 });
+
+async function madrashaServer() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const classesCollection = client.db("madrashaDB").collection("classes");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Madrasha Server successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+madrashaServer().catch(console.dir);
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Madarasha Deen Server Running ${PORT}`);
